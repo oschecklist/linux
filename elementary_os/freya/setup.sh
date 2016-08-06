44f3edf413c130e9ecdbe9e67c61b665820a2d21
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-08-06 13:57:39 -0700

    finished elementary install script

--- elementary_os/freya/setup.sh
+++ elementary_os/freya/setup.sh
@@ -3,17 +3,18 @@
 set -o errexit   # exit on error
 
 # install apps I want
+echo "(If a PPA fails to add, just re-run the script.)"
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo add-apt-repository ppa:kirillshkrogalev/ffmpeg-next -y
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo add-apt-repository ppa:bartbes/love-stable -y
 sudo apt-get update
-sudo apt-get install keepass2 git ncdu htop redshift virtualbox oracle-java8-installer steam ffmpeg obs-studio love screen nano wget curl tree transmission libreoffice gimp gnome-system-monitor wondershaper rar unrar zip unzip bsdgames -y # should already be there: screen, nano, wget, curl, unzip
+sudo apt-get install keepass2 git ncdu htop redshift virtualbox oracle-java8-installer steam ffmpeg obs-studio love screen nano wget curl tree transmission libreoffice gimp gnome-system-monitor wondershaper rar unrar zip unzip bsdgames -y # should already be there: nano, wget, curl, unzip
 
 # Dropbox is special
 git clone https://github.com/zant95/elementary-dropbox/
 cd elementary-dropbox
-bash ./install.sh #TODO figure out if this needs to be launched in a screen
+bash ./install.sh
 cd ..
 
 # LuaRocks is also special
@@ -34,8 +35,12 @@ cd ..
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://github.com/atom/atom/releases/download/v1.9.2/atom-amd64.deb
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.1.0-amd64.deb
-sudo mv *.deb /var/cache/apt/archives/
-sudo apt-get install google-chrome-stable atom slack-desktop -y
+set +e   # ignore errors when unpacking deb's without their dependencies
+sudo dpkg -i google-chrome*.deb
+sudo dpkg -i atom*.deb
+sudo dpkg -i slack-desktop*.deb
+set -e   # and turn stopping on error back on
+sudo apt-get -f install -y
 
 # uninstall apps I don't want
 sudo apt-get purge midori-granite -y
