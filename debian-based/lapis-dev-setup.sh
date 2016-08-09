6fd1390cc2e8ad6d6f98659598b14cae32414f2b
/Fox --DEFCON <paul.liverman.iii@gmail.com>
2016-08-08 22:12:26 -0700

    Made lapis-dev-setup.sh BETTAR

--- debian-based/lapis-dev-setup.sh
+++ debian-based/lapis-dev-setup.sh
@@ -2,20 +2,26 @@
 
 set -o errexit   # exit on error
 
+RESTYVER=1.9.7.5   # CHECK FOR NEW VERSION AT http://openresty.org/en/download.html
+ROCKSVER=2.3.0     # CHECK FOR NEW VERSION AT http://keplerproject.github.io/luarocks/releases/
+
+echo "Please check for new versions of OpenResty and LuaRocks first!"
+read -p " Press [Enter] to continue, or Ctrl+C to cancel."
+
 sudo apt-get update
 sudo apt-get install nginx lua5.1 liblua5.1-0-dev zip unzip libreadline-dev libncurses5-dev libpcre3-dev openssl libssl-dev perl make build-essential postgresql -y
 # OpenResty
-wget https://openresty.org/download/openresty-1.9.7.5.tar.gz   # Install a later version if available!
-tar xvf openresty-1.9.7.5.tar.gz
-cd openresty-1.9.7.5
+wget https://openresty.org/download/openresty-$RESTYVER.tar.gz   # Install a later version if available!
+tar xvf openresty-$RESTYVER.tar.gz
+cd openresty-$RESTYVER
 ./configure
 make
 sudo make install
 cd ..
 # LuaRocks
-wget https://keplerproject.github.io/luarocks/releases/luarocks-2.3.0.tar.gz # Install a later version if available!
-tar xvf luarocks-2.3.0.tar.gz
-cd luarocks-2.3.0
+wget https://keplerproject.github.io/luarocks/releases/luarocks-$ROCKSVER.tar.gz # Install a later version if available!
+tar xvf luarocks-$ROCKSVER.tar.gz
+cd luarocks-$ROCKSVER
 ./configure
 make build
 sudo make install
