ecca4d6ad5243d8f750c353688b7a88b772f0023
/Fox --develop <paul.liverman.iii@gmail.com>
2016-09-07 22:17:29 -0700

    add kid3 to install list

--- elementary_os/freya/setup.sh
+++ elementary_os/freya/setup.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo add-apt-repository ppa:bartbes/love-stable -y
 sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily -y
 sudo apt-get update
-sudo apt-get install keepass2 git ncdu htop redshift virtualbox oracle-java8-installer steam ffmpeg obs-studio love screen nano wget curl tree transmission libreoffice gimp gnome-system-monitor wondershaper rar unrar zip unzip bsdgames elementary-tweaks dconf-editor gparted -y # should already be there: nano, wget, curl, unzip
+sudo apt-get install keepass2 git ncdu htop redshift virtualbox oracle-java8-installer steam ffmpeg obs-studio love screen nano wget curl tree transmission libreoffice gimp gnome-system-monitor wondershaper rar unrar zip unzip bsdgames elementary-tweaks dconf-editor gparted kid3 -y # should already be there: nano, wget, curl, unzip
 
 # Dropbox is special
 git clone https://github.com/zant95/elementary-dropbox/
