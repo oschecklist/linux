39a47d25bc20a8e90d83c0368b059e32910aaf1c
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-12-29 16:01:29 -0800

    fix deb-based update script

--- debian-based/update.sh
+++ debian-based/update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# todo remove this line and make this script exectuable in the repo
-
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get autoremove
-sudo apt-get autoclean
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
